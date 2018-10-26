5c6158edfe2dad015ce4b2974b5d5b540aae6833
ssaranu <ssaranu@opttechcorp.com>
2018-10-25 19:55:48 -0500

    created 3 files for rhel6 and rhel7 to create user

---
+++ recipes/rhel6.rb
@@ -0,0 +1,9 @@
+user 'satya' do
+  comment 'Satya Saranu'
+  uid '4000'
+  gid '5000'
+  home '/home/satya'
+  shell '/bin/bash'
+  password '$6$NxM5C.32vJBQCryl$AWvZ0Hy2nipBgXw.qeAGw.jPvSsNeTbCIx6/B4vWpHg/OcSgKo1S.WI0Bo3KEjMUwaulbrwvZM3.EyM4JCBXe1'
+  action :create
+end
