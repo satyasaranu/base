9c1a0cecd86e5150c3500cfb932d089062df862f
ssaranu <ssaranu@opttechcorp.com>
2018-10-27 15:12:53 -0500

    add logic for resolv.conf

--- recipes/default.rb
+++ recipes/default.rb
@@ -4,11 +4,16 @@
 #
 # Copyright:: 2018, The Authors, All Rights Reserved.
 
-case node['platform']
-when "redhat"
+include_recipe "base::rhel7"
+include_recipe "base::rhel6"
+case node['platform_family']
+when "rhel"
     if node['platform_version'].to_f >= 7.0
         include_recipe "base::rhel7"
     else
         include_recipe "base::rhel6"
     end
 end
+
+case 
+
